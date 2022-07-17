43619e69680523899bc8def3779410c6702d8bf9
Gaeon <gaeon0413@gmail.com>
2022-07-17 16:34:42 +0900

    libft

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 16:03:48 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/07/09 17:25:55 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/07/17 16:17:06 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	*ft_memcpy(void *dst, void *src, size_t n)
 {
-	int		cnt;
+	size_t	cnt;
 	char	*temp_dst;
 	char	*temp_src;
 
 	cnt = 0;
+	if (!n || dst == src)
+		return (dst);
 	temp_dst = (char *)dst;
 	temp_src = (char *)src;
 	while (cnt < n)
--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:13:44 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/07/09 17:34:51 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/07/17 16:26:07 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	int		cnt;
+	size_t	cnt;
 	char	*temp_dst;
 	char	*temp_src;
 
-	cnt = 0;
+	cnt = -1;
 	temp_dst = (char *)dst;
 	temp_src = (char *)src;
-	cnt = 0;
-	while (cnt < len)
-	{
-		temp_dst[cnt] = temp_src[cnt];
-		cnt++;
-	}
+	if (!dst && !src)
+		return (0);
+	if (temp_dst < temp_src)
+		while (++cnt < len)
+			temp_dst[cnt] = temp_src[cnt];
+	else
+		while (++cnt < len)
+			temp_dst[len - cnt - 1] = temp_src[len - cnt - 1];
 	return (temp_dst);
 }
