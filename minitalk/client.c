646141d88fa72486638116b0bfe369daa1a96018
Gaeon <gaeon0413@gmail.com>
2022-11-19 02:14:33 +0900

    minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -5,13 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/09 16:35:30 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/10/31 16:24:08 by gaeokim          ###   ########.fr       */
+/*   Created: 2022/11/10 17:21:42 by gaeokim           #+#    #+#             */
+/*   Updated: 2022/11/19 02:06:54 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "minitalk.h"
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int		idx;
+	int		sign;
+	long	ret;
+
+	idx = 0;
+	sign = 1;
+	ret = 0;
+	while ((9 <= str[idx] && str[idx] <= 13) || str[idx] == 32)
+		idx++;
+	if (str[idx] == '+' || str[idx] == '-')
+	{
+		if (str[idx] == '-')
+			sign = -1;
+		idx++;
+	}
+	while ('0' <= str[idx] && str[idx] <= '9')
+	{
+		ret = ret * 10 + str[idx] - '0';
+		if (sign == 1 && ret > 2147483647)
+			return (-1);
+		else if (sign == -1 && ret > 2147483648)
+			return (0);
+		idx++;
+	}
+	return ((int)sign * ret);
+}
 
 void	client(pid_t pid, char *str)
 {
@@ -23,12 +54,19 @@ void	client(pid_t pid, char *str)
 		while (idx < 8)
 		{
 			if ((*str >> idx & 1) == 1)
+			{
 				kill(pid, SIGUSR1);
+				usleep(10);
+			}
 			else
+			{
 				kill(pid, SIGUSR2);
+				usleep(10);
+			}
 			idx++;
 		}
 		str++;
+		usleep(10);
 	}
 }
 
@@ -43,3 +81,16 @@ int	main(int argc, char *argv[])
 	}
 	return (0);
 }
+
+// int	main(int argc, char *argv[])
+// {
+// 	pid_t	pid;
+
+// 	if (argc == 2)
+// 	{
+// 		pid = atoi(argv[1]);
+// 		kill(pid, SIGUSR1);
+// 		kill(pid, SIGUSR2);
+// 	}
+// 	return (0);
+// }
--- minitalk/server.c
+++ minitalk/server.c
@@ -5,45 +5,95 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/19 13:09:21 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/11/10 17:43:51 by gaeokim          ###   ########.fr       */
+/*   Created: 2022/09/25 14:37:06 by gaeokim           #+#    #+#             */
+/*   Updated: 2022/11/19 02:14:22 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
 
-static t_info	g_info;
+// static int g_cnt;
+// static int g_num;
 
-void	init(void)
+// void	sig_handler(int signo)
+// {
+// 	if (signo == SIGUSR1 && g_cnt < 8)
+// 		g_num = g_num * 2 + 1;
+// 	else if (signo == SIGUSR2 && g_cnt < 8)
+// 		g_num = g_num * 2;
+// 	printf("%d", g_num);
+// 	g_cnt++;
+// 	if (g_cnt == 8)
+// 		printf("%d", g_num);
+// }
+
+static int	g_cnt;
+static int	g_num;
+
+void	sig_handler(int signo)
 {
-	ft_printf("server pid : %d\n", getpid());
-	g_info.cnt = 0;
-	g_info.c = 0;
-}
+	int	n;
+	int	m;
 
-void	ft_handler(int signum)
-{
-	if (signum == SIGUSR1)
-		g_info.c = g_info.c * 2 + 1;
-	else
-		g_info.c = g_info.c * 2;
-	g_info.cnt++;
-	if (g_info.cnt == 8)
+	n = 0;
+	m = 1;
+	while (n < g_cnt)
 	{
-		ft_printf("%c", g_info.c);
-		g_info.cnt = 0;
-		g_info.c = 0;
+		m *= 2;
+		n++;
+	}
+	if (signo == SIGUSR1)
+		g_num += m;
+	else if (signo == SIGUSR2)
+		;
+	g_cnt++;
+	if (g_cnt == 8)
+	{
+		printf("%c\n", g_num);
+		g_cnt = 0;
+		g_num = 0;
 	}
 }
 
 int	main(void)
 {
-	init();
-	if (signal(SIGUSR1, (void *)ft_handler))
-		;
-	if (signal(SIGUSR2, (void *)ft_handler))
+	printf("server pid : %d\n", getpid());
+	if (signal(SIGUSR1, (void *)sig_handler)
+		|| signal(SIGUSR2, (void *)sig_handler))
 		;
 	while (1)
-		pause();
+		usleep(1);
 	return (0);
 }
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char	c;
+
+// 	c = 'a';
+// 	for (int i = 7 ; i >= 0 ; --i)
+// 	{
+// 		printf("%d", c >> i & 1);
+// 	}
+// 	printf("\n");
+// 	for (int j = 7 ; j >= 0 ; --j)
+// 	{
+// 		printf("%d ", c >> j);
+// 	}
+// }
+
+// 01100001
+
+// 00000000 0 
+// 00000001 1 
+// 00000011 3 
+// 00000110 6 
+// 00001100 12
+// 00011000 24
+// 00110000 48
+// 01100001 97
+// 1 2 4 8 16 32 64
