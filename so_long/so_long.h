58dd13050fb9a9c12c3fb3607254203f94a2772d
Gaeon <gaeon0413@gmail.com>
2022-11-26 21:27:30 +0900

    so_long

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 15:54:12 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/11/20 16:41:37 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/11/26 21:21:55 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define SO_LONG_H
 
 # include "mlx/mlx.h"
+# include "mlx_opengl/mlx.h"
+# include "mlx_mms/mlx.h"
 
 #endif
