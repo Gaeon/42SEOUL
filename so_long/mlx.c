0736d1fe5c4ac97b3f4495229163a77620f0aa84
Gaeon <gaeon0413@gmail.com>
2022-12-29 17:01:16 +0900

    mlx

---
+++ so_long/mlx.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 16:07:45 by gaeokim           #+#    #+#             */
+/*   Updated: 2022/12/29 16:56:52 by gaeokim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	key_press(int keycode, t_game game, t_player *param)
+{
+	static int	a = 0;
+
+	if (keycode == KEY_W)
+		param->position -= game.width + 1;
+	else if (keycode == KEY_S)
+		param->position += game.width + 1;
+	else if (keycode == KEY_A)
+		param->position -= 1;
+	else if (keycode == KEY_D)
+		param->position += 1;
+	else if (keycode == KEY_ESC)
+		exit(0);
+	return (0);
+}
