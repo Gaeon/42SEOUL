bbafd670d793022edd8fedb49977557613a6b592
Gaeon <gaeon0413@gmail.com>
2023-01-08 16:59:56 +0900

    mlx

--- so_long/mlx.c
+++ so_long/mlx.c
@@ -6,23 +6,47 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 16:07:45 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/12/29 18:06:18 by gaeokim          ###   ########.fr       */
+/*   Updated: 2023/01/08 16:57:53 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	key_press(int keycode, t_game game, t_player *param)
+int	key_press(int keycode, t_game *game, t_player *param)
 {
+	if (keycode == KEY_ESC)
+		exit_game(game);
 	if (keycode == KEY_W)
-		param->position -= game.width + 1;
-	else if (keycode == KEY_S)
-		param->position += game.width + 1;
-	else if (keycode == KEY_A)
-		param->position -= 1;
-	else if (keycode == KEY_D)
-		param->position += 1;
-	else if (keycode == KEY_ESC)
-		exit(0);
+		move_w(game, param);
+	if (keycode == KEY_A)
+		move_a(game, param);
+	if (keycode == KEY_S)
+		move_s(game, param);
+	if (keycode == KEY_D)
+		move_d(game, param);
 	return (0);
+}
+
+void	move_w(t_game *game, t_player *param)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < ft_strlen(game->map))
+	{
+		if (game->map[i] == 'P')
+			break ;
+	}
+	if (game->map[i - game->width] == 'C')
+		param->collect++;
+	if (game->map[i - game->width] == 'E' && game->collect == param->collect)
+		clear_game(game);
+	else if (game->map[i - game->width] != '1' && game->map[i - game->width] != 'E')
+	{
+		game->map[i] = '0';
+		game->map[i - game->width] = 'P';
+		param->move++;
+		ft_printf("%d\n", param->move);
+		draw_map(game, img);
+	}
 }
