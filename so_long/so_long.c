6cf8d96fb2fbf45f74402761e2be3e67038e24b4
Gaeon <gaeon0413@gmail.com>
2023-01-08 18:49:42 +0900

    so_long

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 16:14:10 by gaeokim           #+#    #+#             */
-/*   Updated: 2023/01/08 16:47:43 by gaeokim          ###   ########.fr       */
+/*   Updated: 2023/01/08 17:12:19 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	main(int argc, char *argv[])
 			game.width * 64, game.height * 64, "so_long");
 	init_param(&game, &param);
 	init_image(game, &img);
-	mlx_hook(game.win_ptr, X_EVENT_KEY_RELEASE, 0, &key_press, &param);
+	// mlx_hook(game.win_ptr, X_EVENT_KEY_RELEASE, 0, &key_press, &param);
 	draw_map(game, img);
 	mlx_loop(game.mlx_ptr);
 	return (0);
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 19:32:54 by gaeokim           #+#    #+#             */
-/*   Updated: 2023/01/08 16:57:11 by gaeokim          ###   ########.fr       */
+/*   Updated: 2023/01/08 18:45:59 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,10 +83,13 @@ int		map_check(t_game *game);
 void	draw_map(t_game game, t_image img);
 
 //mlx.c
-int		key_press(int keycode, t_game *game, t_player *param);
-void	move_w(t_game *game, t_player *param);
-void	move_a(t_game *game, t_player *param);
-void	move_s(t_game *game, t_player *param);
-void	move_d(t_game *game, t_player *param);
+int		key_press(int keycode, t_game *game, t_player *param, t_image img);
+void	move_w(t_game *game, t_player *param, t_image img);
+void	move_a(t_game *game, t_player *param, t_image img);
+void	move_s(t_game *game, t_player *param, t_image img);
+void	move_d(t_game *game, t_player *param, t_image img);
+
+void	exit_game(t_game *game);
+void	clear_game(t_game *game);
 
 #endif
