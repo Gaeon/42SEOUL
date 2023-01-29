bd40ab395bb3ea90a45e57286bb4510dc62a1203
Gaeon <gaeon0413@gmail.com>
2023-01-29 14:18:12 +0900

    util

---
+++ push_swap/util.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   util.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 13:08:43 by gaeokim           #+#    #+#             */
+/*   Updated: 2023/01/18 13:17:26 by gaeokim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_atoi(const char *str)
+{
+	int		idx;
+	int		sign;
+	long	ret;
+
+	idx = 0;
+	sign = 1;
+	ret = 0;
+	while ((9 <= str[idx] && str[idx] <= 13) || str[idx] == 32)
+		idx++;
+	if (str[idx] == '+' || str[idx] == '-')
+	{
+		if (str[idx] == '-')
+			sign = -1;
+		idx++;
+	}
+	while ('0' <= str[idx] && str[idx] <= '9')
+	{
+		ret = ret * 10 + str[idx] - '0';
+		if (sign == 1 && ret > 2147483647)
+			return (-1);
+		else if (sign == -1 && ret > 2147483648)
+			return (0);
+		idx++;
+	}
+	return ((int)sign * ret);
+}
