b64e93f0a69920ac940d87d00bbf096f329a6660
Gaeon <gaeon0413@gmail.com>
2023-04-02 18:54:40 +0900

    fix loop

--- minishell/sources/utils/ft_update_user.c
+++ minishell/sources/utils/ft_update_user.c
@@ -6,7 +6,7 @@
 /*   By: gaeon <gaeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 16:02:43 by johyyoon          #+#    #+#             */
-/*   Updated: 2023/04/02 18:43:02 by gaeon            ###   ########.fr       */
+/*   Updated: 2023/04/02 18:54:29 by gaeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,12 @@ void	ft_update_user(char ***user, int fd)
 	while (1)
 	{
 		line = get_next_line(fd);
-		if (line != NULL)
-		{
-			trimmed_line = ft_strtrim(line, "\n");
-			free(line);
-			new_user = ft_extend_split(new_user, trimmed_line);
-			free(trimmed_line);
-		}
+		if (!line)
+			break ;
+		trimmed_line = ft_strtrim(line, "\n");
+		free(line);
+		new_user = ft_extend_split(new_user, trimmed_line);
+		free(trimmed_line);
 	}
 	ft_free_split(user);
 	*user = new_user;
