d736edc1979696e179c1ab9a6a449a752ea38f9f
Gaeon <gaeon0413@gmail.com>
2023-04-02 16:18:13 +0900

    minishell

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaeon <gaeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/10 11:44:46 by sangyeki          #+#    #+#             */
-/*   Updated: 2023/04/02 15:00:37 by gaeon            ###   ########.fr       */
+/*   Created: 2023/03/04 16:14:00 by johyyoon          #+#    #+#             */
+/*   Updated: 2023/04/02 16:18:01 by gaeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@
 # include <sys/ioctl.h>
 # include <dirent.h>
 # include <signal.h>
+
 # include "../libft/libft.h"
 # include "get_next_line.h"
+
 # include "readline/history.h"
 # include "readline/readline.h"
 
@@ -64,10 +66,12 @@ enum	e_error
 
 int			g_status_number;
 
+// srcs/bulit
 int			ft_built(t_cmd *cmd);
 int			ft_built_in(t_data *data, t_list *cmd, int *exit_n, int n);
 int			ft_built_in_2(t_data *data, t_list *cmd, char **str, int n);
 
+// srcs/parsing
 char		**ft_cmd_split(char *in_cmd, char *set);
 char		*ft_expand_cmd(char *in_cmd, int i, int q[2], t_data *data);
 char		*ft_expand_path(char *in_cmd, int i, int q[2], char *var);
@@ -85,6 +89,43 @@ char		**ft_split_cmd(const char *in_cmd, char *c);
 int			ft_word_count(char *in_cmd, char *set, int count);
 int			ft_cmd_count(const char *in_cmd, char *set, int c[2]);
 
+// srcs/cmd
+t_cmd		*ft_get_infile(t_cmd *cmd, char **tmp, int *i);
+t_cmd		*ft_get_infile_2(t_cmd *cmd, char **tmp, int *i);
+t_cmd		*ft_get_outfile(t_cmd *cmd, char **tmp, int *i);
+t_cmd		*ft_get_outfile_2(t_cmd *cmd, char **tmp, int *i);
+int			ft_check_dir(char c);
+int			ft_getfd(int o_fd, char *path, int f[2]);
+int			ft_heredoc(char *str2[2], char *str[2]);
+char		*ft_heredoc_2(char *str2[2], size_t len, char *limit, char *warn);
+int			ft_cd(t_data *data);
+void		ft_cd_error(char **str[2]);
+DIR			*ft_check_cmd(t_data *data, t_list *cmd, char ***str, char *path);
+int			ft_echo(t_list *cmd);
+char		*ft_find_cmd(char **str, char *cmd, char *f_path);
+int			ft_pwd(void);
+int			ft_exit(t_list *cmd, int *exit_n);
+int			ft_export(t_data *data);
+t_data		*ft_export_2(t_data *data, char **str, int q[3], int *r_n);
+int			ft_exp_error(int q[3], int *r_n);
+int			ft_ch_env_name(char *str);
+int			ft_unset(t_data *data);
+int			ft_cmd_export(t_data *data);
+
+// srcs/pipex
+void		*ft_check_fork(t_data *data, t_list *cmd, int fd[2]);
+void		ft_fork(t_data *data, t_list *cmd, int fd[2]);
+void		ft_fork_2(t_data *data, t_list *cmd, int fd[2]);
+void		*ft_fork_3(t_list *cmd, int fd[2]);
+void		ft_fork_4(t_data *data, t_cmd *cmd_c, int len, t_list *cmd);
+void		ft_get_cmd(t_data *data, t_list *cmd, char **str, char *path);
+void		*ft_run_cmd(t_data *data, t_list *cmd);
+
+// srcs/signal
+void		ft_sig_q(int sig);
+void		ft_sig_c(int sig);
+
+// srcs/utils
 long long	ft_atol(char *str, long long *n);
 void		*ft_check_ag(char *in_cmd, t_data *data);
 int			ft_count_len(char *s1);
@@ -113,38 +154,5 @@ int			ft_count_char(char *str, char c);
 int			ft_cmd_env(char **env, int new_line, int fd);
 int			ft_env_var(char *ag, char **env, int q[2]);
 char		*ft_find_str(char *str);
-
-t_cmd		*ft_get_infile(t_cmd *cmd, char **tmp, int *i);
-t_cmd		*ft_get_infile_2(t_cmd *cmd, char **tmp, int *i);
-t_cmd		*ft_get_outfile(t_cmd *cmd, char **tmp, int *i);
-t_cmd		*ft_get_outfile_2(t_cmd *cmd, char **tmp, int *i);
-int			ft_check_dir(char c);
-int			ft_getfd(int o_fd, char *path, int f[2]);
-int			ft_heredoc(char *str2[2], char *str[2]);
-char		*ft_heredoc_2(char *str2[2], size_t len, char *limit, char *warn);
-int			ft_cd(t_data *data);
-void		ft_cd_error(char **str[2]);
-DIR			*ft_check_cmd(t_data *data, t_list *cmd, char ***str, char *path);
-int			ft_echo(t_list *cmd);
-char		*ft_find_cmd(char **str, char *cmd, char *f_path);
-int			ft_pwd(void);
-int			ft_exit(t_list *cmd, int *exit_n);
-int			ft_export(t_data *data);
-t_data		*ft_export_2(t_data *data, char **str, int q[3], int *r_n);
-int			ft_exp_error(int q[3], int *r_n);
-int			ft_ch_env_name(char *str);
-int			ft_unset(t_data *data);
-int			ft_cmd_export(t_data *data);
-
-void		*ft_check_fork(t_data *data, t_list *cmd, int fd[2]);
-void		ft_fork(t_data *data, t_list *cmd, int fd[2]);
-void		ft_fork_2(t_data *data, t_list *cmd, int fd[2]);
-void		*ft_fork_3(t_list *cmd, int fd[2]);
-void		ft_fork_4(t_data *data, t_cmd *cmd_c, int len, t_list *cmd);
-void		ft_get_cmd(t_data *data, t_list *cmd, char **str, char *path);
-void		*ft_run_cmd(t_data *data, t_list *cmd);
-
-void		ft_sig_q(int sig);
-void		ft_sig_c(int sig);
 
 #endif
